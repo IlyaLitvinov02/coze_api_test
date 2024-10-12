ac4ad5fb01de76c5c4d6aca0160e20f162453145
Ilya Litvinov <ilya.litvinov@twill.health>
2024-10-13 02:11:10 +0600

    added create-message endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/create-message')
+  handleSendPulseWebhook({userId, text}: {
+    userId: string
+    text: string
+  }){
+    this.appService.handleSendPulseWebhook({userId, text})
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,33 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) { }
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async handleSendPulseWebhook({ userId, text }: {
+    text: string
+    userId: string
+  }) {
+    const response = await lastValueFrom(
+      this.httpService.post("https://api.coze.com/v3/chat",
+        {
+          stream: true,
+          bot_id: '7368357412157489158',
+          user_id: userId,
+          additional_messages: [{ role: 'user', content_type: 'text', content: text }]
+        },
+        {
+          headers: {
+            "Authorization": "Bearer pat_OdgLSrNrR9XzTpf8XJTsu3yVXxcmzoCK4fTO5Cj9tyuTF8FNBlC9GGJLUQzFPhs2",
+            "Content-Type": "application/json",
+          }
+        }
+      ))
+    return response
   }
 }
